91a61b7270dec225f8d5e70d107d4e8f0a80908b
salviosage <salviosage@gmail.com>
2020-01-26 09:42:30 -0800

    finish all for scope and closure

---
+++ scope and closure/IFFEs.js
@@ -0,0 +1,9 @@
+
+//IFFEs (Immediately-Invoked Function Expression)
+var name ="adventist university of cenntral africa";
+( function Gishushucampus(){
+  var name="AUCA Gishushu Campus";
+  console.log(name);
+ 
+})();
+  console.log(name);
